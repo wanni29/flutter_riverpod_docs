f53dd8c3ef466d6e15638afed1e88be33da18f03
wanni30 <hubhy@nate.com>
2024-11-22 00:42:31 +0900

    copyWith 개념 잡기

--- lib/lab_3/model/counter.dart
+++ lib/lab_3/model/counter.dart
@@ -8,6 +8,7 @@ class Counter {
   });
 
   // copywith에 대한 코드 개념 및 사용법 정리
+  // -> 기존에 사용하던 객체는 그대로 두고, 새로운 객체가 생성되지만 필드값이 조금씩 다르게 생성됨
   Counter copyWith({int? count, String? messageId}) {
     return Counter(
       count: count ?? this.count,
